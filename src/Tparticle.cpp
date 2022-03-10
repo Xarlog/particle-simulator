0691fad01588ac74786bc1442e39d1735e8715da
Xarlog <101275079+Xarlog@users.noreply.github.com>
2022-03-10 22:28:14 +0100

    Added particle color

--- src/Tparticle.cpp
+++ src/Tparticle.cpp
@@ -18,6 +18,16 @@ Tparticle::Tparticle(vector3d<double> Position, vector3d<double> Velocity, doubl
     repulsive_charge=Repulsive_charge;
     attractive_charge=Attractive_charge;
 }
+Tparticle::Tparticle(vector3d<double> Position, vector3d<double> Velocity, double Mass, double Electric_charge, double Repulsive_charge, double Attractive_charge, vector3d<double> Color)
+{
+    position=Position;
+    velocity=Velocity;
+    mass=Mass;
+    electric_charge=Electric_charge;
+    repulsive_charge=Repulsive_charge;
+    attractive_charge=Attractive_charge;
+    color=Color;
+}
 Tparticle::~Tparticle()
 {
     //dtor
